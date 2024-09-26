4ec4b7a025d8dbfea93be2c55d6d16c893f539a9
Bito Kouki <t7dd23336@stu.tamagawa.ac.jp>
2024-09-26 16:10:18 -0400

    creat seedDB

--- seeds/index.js
+++ seeds/index.js
@@ -1,6 +1,7 @@
 const mongoose=require("mongoose");
 const Campground=require("../models/campground");
 const cities=require("./cities");
+const {descriptors,places}=require("./seedHelpers");
 mongoose.connect("mongodb://localhost:27017/yelp-camp",
     { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
     .then(() => {
@@ -10,17 +11,22 @@ mongoose.connect("mongodb://localhost:27017/yelp-camp",
         console.log('MongoDBコネクションエラー！！！');
         console.log(err);
     });
-  
+const sample=(array)=>array[Math.floor(Math.random()*array.length)]
+
+
   const seedDB=async ()=>{
     await Campground.deleteMany({});
     for(let i=0;i<50;i++){
         const randomCityIndex=Math.floor(Math.random()*cities.length);
         const randomCity=cities[randomCityIndex];
         const camp=new Campground({
-            location:`${randomCity.prefecture}${randomCity.city}`
+            location:`${randomCity.prefecture}${randomCity.city}`,
+            title:`${sample(descriptors)}・${sample(places)}`
         });
         await camp.save();
     }
 
   }
-  seedDB();
+  seedDB().then(()=>{
+    mongoose.connection.close();
+  });
