f31db844e8f2d489c02676b139d5ad3428a1aaf8
Bito Kouki <t7dd23336@stu.tamagawa.ac.jp>
2024-09-27 13:47:52 -0400

    creat funcation of yelp-camp CRUD

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 const express=require("express");
 const mongoose = require('mongoose');
 const path=require("path");
-
+const methodOverride = require('method-override')
 const app=express();
 const Campground = require('./models/campground');
+const campground = require("./models/campground");
 const portNumber=3000;
 
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
 
 
+app.use(express.urlencoded({extended:true}));
+app.use(methodOverride('_method'))
 mongoose.connect("mongodb://localhost:27017/yelp-camp",
   { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
   .then(() => {
@@ -20,26 +23,47 @@ mongoose.connect("mongodb://localhost:27017/yelp-camp",
       console.log(err);
   });
 
-
-
-
-
-
-
 app.get("/",(req,res)=>{
     res.render("home")
 })
-
-app.get("/makecampground",async(req,res)=>{
-    const camp= new Campground ({
-        title:`私の庭`,
-        description:`気軽に`
-    })
-    await camp.save();
-    res.send("キャンプ")
+app.get(`/campgrounds`,async(req,res)=>{
+    const campgrounds=await Campground.find({});
+    res.render(`campgrounds/index`,{campgrounds});
+})
+app.get(`/campgrounds/new`,async(req,res)=>{
+    res.render("campgrounds/new")
 })
 
-
+app.get(`/campgrounds/:id`,async(req,res)=>{
+    const campground=await Campground.findById(req.params.id)
+    res.render("campgrounds/show",{campground});
+})
+app.get(`/campgrounds/:id/edit`,async(req,res)=>{
+    const campground=await Campground.findById(req.params.id)
+    res.render("campgrounds/edit",{campground});
+})
+app.post(`/campgrounds`,async(req,res)=>{
+    
+    const campground=new Campground(
+        req.body.campgrounds
+    );
+  
+    await campground.save();
+    res.redirect(`campgrounds/${campground._id}`)
+    
+})
+app.put(`/campgrounds/:id`,async(req,res)=>{
+    const {id}=req.params;
+    const campground=await Campground.findByIdAndUpdate(id,{
+        ...req.body.campgrounds
+    })
+    res.redirect(`/campgrounds/${campground._id}`);
+})
+app.delete(`/campgrounds/:id`,async(req,res)=>{
+    const {id}=req.params;
+    await Campground.findByIdAndDelete(id)
+    res.redirect("/campgrounds")
+})
 app.listen(portNumber,()=>{
     console.log(`${portNumber}でサーバー起動`)
 })
